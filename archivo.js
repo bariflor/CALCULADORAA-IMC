9a644320384d1348518933edf54aba5e3f099e34
bariflor <barihuamachuco@gmail.com>
2017-07-17 13:32:27 -0500

    modificacion de imc

--- archivo.js
+++ archivo.js
@@ -1,4 +1,9 @@
-var peso = prompt("Dime tu talla", "50");
-var talla = prompt("Dime tu talla", "150");
-
+var peso = parseInt(prompt("Dime tu peso", "50"));
+var talla = parseInt(prompt("Dime tu talla en metros", "150"));
 // la formula para hallar IMC= PESO/TALLA *TALLA
+function imc (peso,talla){
+    var nuevaTalla= talla/100;
+    var formula= peso/(nuevaTalla*nuevaTalla);
+    alert("Hola! Tu indice de masa corporal es: " + formula);
+}
+imc(peso,talla)
